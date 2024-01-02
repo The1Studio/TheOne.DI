5205953af33af860630a7843f44268b038a39757
Manh Nguyen <manhnb1911@gmail.com>
2024-01-02 14:40:06 +0700

    .

--- Scripts/DependencyContainer.cs
+++ Scripts/DependencyContainer.cs
@@ -6,33 +6,42 @@ namespace UniT.DI
     using System.Linq;
     using System.Reflection;
 
-    public static class DependencyContainer
+    public sealed class DependencyContainer
     {
+        #region Constructor
+
+        public DependencyContainer()
+        {
+            this.Add(this);
+        }
+
+        #endregion
+
         #region Manual Add
 
-        public static void Add(Type type, object instance)
+        public void Add(Type type, object instance)
         {
-            GetCache(type).Add(instance);
+            this.GetCache(type).Add(instance);
         }
 
-        public static void Add(object instance)
+        public void Add(object instance)
         {
-            Add(instance.GetType(), instance);
+            this.Add(instance.GetType(), instance);
         }
 
-        public static void AddInterfaces(object instance)
+        public void AddInterfaces(object instance)
         {
             foreach (var @interface in instance.GetType().GetInterfaces())
             {
-                Add(@interface, instance);
+                this.Add(@interface, instance);
             }
         }
 
-        public static void AddInterfacesAndSelf(object instance)
+        public void AddInterfacesAndSelf(object instance)
         {
             foreach (var @interface in instance.GetType().GetInterfaces().Append(instance.GetType()))
             {
-                Add(@interface, instance);
+                this.Add(@interface, instance);
             }
         }
 
@@ -40,80 +49,80 @@ namespace UniT.DI
 
         #region Auto Add
 
-        public static void Add(Type type)
+        public void Add(Type type)
         {
-            Add(type, Instantiate(type));
+            this.Add(type, this.Instantiate(type));
         }
 
-        public static void AddInterfaces(Type type)
+        public void AddInterfaces(Type type)
         {
-            AddInterfaces(Instantiate(type));
+            this.AddInterfaces(this.Instantiate(type));
         }
 
-        public static void AddInterfacesAndSelf(Type type)
+        public void AddInterfacesAndSelf(Type type)
         {
-            AddInterfacesAndSelf(Instantiate(type));
+            this.AddInterfacesAndSelf(this.Instantiate(type));
         }
 
         #endregion
 
         #region Get
 
-        public static object Get(Type type)
+        public object Get(Type type)
         {
-            return GetOrDefault(type) ?? throw new Exception($"No instance found for {type.Name}");
+            return this.GetOrDefault(type) ?? throw new Exception($"No instance found for {type.Name}");
         }
 
-        public static object GetOrDefault(Type type)
+        public object GetOrDefault(Type type)
         {
-            return GetCache(type).SingleOrDefault();
+            return this.GetCache(type).SingleOrDefault();
         }
 
-        public static object[] GetAll(Type type)
+        public object[] GetAll(Type type)
         {
-            return GetCache(type).ToArray();
+            return this.GetCache(type).ToArray();
         }
 
         #endregion
 
         #region Instantiate
 
-        public static object Instantiate(Type type)
+        public object Instantiate(Type type)
         {
             if (type.IsAbstract) throw new Exception($"Cannot instantiate abstract type {type.Name}");
             if (type.ContainsGenericParameters) throw new Exception($"Cannot instantiate generic type {type.Name}");
             var ctor = type.GetConstructors().SingleOrDefault()
                 ?? throw new Exception($"No constructor found for {type.Name}");
-            return ctor.Invoke(ResolveParameters(ctor.GetParameters(), $"instantiating {type.Name}"));
+            return ctor.Invoke(this.ResolveParameters(ctor.GetParameters(), $"instantiating {type.Name}"));
         }
 
-        public static object Invoke(object obj, string methodName)
+        public object Invoke(object obj, string methodName)
         {
             var method = obj.GetType().GetMethod(methodName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
                 ?? throw new Exception($"Method {methodName} not found on {obj.GetType().Name}");
-            return method.Invoke(obj, ResolveParameters(method.GetParameters(), $"invoking {methodName} on {obj.GetType().Name}"));
+            return method.Invoke(obj, this.ResolveParameters(method.GetParameters(), $"invoking {methodName} on {obj.GetType().Name}"));
         }
 
         #endregion
 
         #region Resolve
 
-        private static readonly ReadOnlyCollection<Type> SupportedInterfaces = new ReadOnlyCollection<Type>(new[] { typeof(IEnumerable<>), typeof(ICollection<>), typeof(IList<>), typeof(IReadOnlyCollection<>), typeof(IReadOnlyList<>) });
+        private readonly ReadOnlyCollection<Type> supportedInterfaces = new ReadOnlyCollection<Type>(new[] { typeof(IEnumerable<>), typeof(ICollection<>), typeof(IList<>), typeof(IReadOnlyCollection<>), typeof(IReadOnlyList<>) });
 
-        private static readonly ReadOnlyCollection<Type> SupportedConcreteTypes = new ReadOnlyCollection<Type>(new[] { typeof(Collection<>), typeof(List<>), typeof(ReadOnlyCollection<>) });
+        private readonly ReadOnlyCollection<Type> supportedConcreteTypes = new ReadOnlyCollection<Type>(new[] { typeof(Collection<>), typeof(List<>), typeof(ReadOnlyCollection<>) });
 
-        private static object[] ResolveParameters(ParameterInfo[] parameters, string context)
+        private object[] ResolveParameters(ParameterInfo[] parameters, string context)
         {
             return parameters.Select(parameter =>
             {
                 var parameterType = parameter.ParameterType;
                 switch (parameterType)
                 {
-                    case { IsGenericType: true, IsInterface: true } when SupportedInterfaces.Contains(parameterType.GetGenericTypeDefinition()):
+                    case { IsGenericType: true, IsInterface: true } when this.supportedInterfaces.Contains(parameterType.GetGenericTypeDefinition()):
                     {
                         return GetArray(parameterType.GetGenericArguments()[0]);
                     }
-                    case { IsGenericType: true } when SupportedConcreteTypes.Contains(parameterType.GetGenericTypeDefinition()):
+                    case { IsGenericType: true } when this.supportedConcreteTypes.Contains(parameterType.GetGenericTypeDefinition()):
                     {
                         return Activator.CreateInstance(parameterType, GetArray(parameterType.GetGenericArguments()[0]));
                     }
@@ -123,16 +132,16 @@ namespace UniT.DI
                     }
                     default:
                     {
-                        var instance = GetOrDefault(parameterType);
+                        var instance = this.GetOrDefault(parameterType);
                         if (instance is null && !parameter.HasDefaultValue) throw new Exception($"Cannot resolve {parameterType.Name} for {parameter.Name} while {context}");
                         return instance ?? parameter.DefaultValue;
                     }
                 }
             }).ToArray();
 
-            static Array GetArray(Type type)
+            Array GetArray(Type type)
             {
-                var instances = GetAll(type);
+                var instances = this.GetAll(type);
                 var array     = Array.CreateInstance(type, instances.Length);
                 instances.CopyTo(array, 0);
                 return array;
@@ -143,32 +152,34 @@ namespace UniT.DI
 
         #region Generic
 
-        public static void Add<T>(T instance) => Add(typeof(T), instance);
+        #if NET_STANDARD_2_1
+        public void Add<T>(T instance) => this.Add(typeof(T), instance);
 
-        public static void Add<T>() => Add(typeof(T));
+        public void Add<T>() => this.Add(typeof(T));
 
-        public static void AddInterfaces<T>() => AddInterfaces(typeof(T));
+        public void AddInterfaces<T>() => this.AddInterfaces(typeof(T));
 
-        public static void AddInterfacesAndSelf<T>() => AddInterfacesAndSelf(typeof(T));
+        public void AddInterfacesAndSelf<T>() => this.AddInterfacesAndSelf(typeof(T));
 
-        public static T Get<T>() => (T)Get(typeof(T));
+        public T Get<T>() => (T)this.Get(typeof(T));
 
-        public static T GetOrDefault<T>() => (T)GetOrDefault(typeof(T));
+        public T GetOrDefault<T>() => (T)this.GetOrDefault(typeof(T));
 
-        public static T[] GetAll<T>() => GetAll(typeof(T)).Cast<T>().ToArray();
+        public T[] GetAll<T>() => this.GetAll(typeof(T)).Cast<T>().ToArray();
 
-        public static T Instantiate<T>() => (T)Instantiate(typeof(T));
+        public T Instantiate<T>() => (T)this.Instantiate(typeof(T));
+        #endif
 
         #endregion
 
         #region Cache
 
-        private static readonly Dictionary<Type, HashSet<object>> Cache = new Dictionary<Type, HashSet<object>>();
+        private readonly Dictionary<Type, HashSet<object>> cache = new Dictionary<Type, HashSet<object>>();
 
-        private static HashSet<object> GetCache(Type type)
+        private HashSet<object> GetCache(Type type)
         {
-            if (!Cache.ContainsKey(type)) Cache.Add(type, new HashSet<object>());
-            return Cache[type];
+            if (!this.cache.ContainsKey(type)) this.cache.Add(type, new HashSet<object>());
+            return this.cache[type];
         }
 
         #endregion
